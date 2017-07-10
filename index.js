ca23e8aeb4c43a637d573ecdee98573a859c90b7
Jonathan Sax <jsax8310@outlook.com>
2017-07-10 13:07:46 -0600

    third commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ server.get('/', function (request, response) {
   response.send('Go to /jonathan to find out more about me')
 })
 
+server.get('/jonathan', function (request, response) {
+  response.json(jonathan)
+})
+
 server.get('*', function (request, response) {
   response.status(400)
   response.send('OH NO!!!!')
